b3bdf67d5b7c7792e013268a1320e2f8107513b1
goratsy <gorpetros06@gmail.com>
2024-01-25 00:18:37 +0300

    adding Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,58 +1,57 @@
-// import * as React from 'react';
-// import {AppBar, Button, IconButton, Toolbar, Typography} from '@mui/material';
-
-// export default function Navbar() {
-//   return (
-//     <>
-//       {/* <AppBar>
-//         <Toolbar>
-//           <IconButton>
-
-//           </IconButton>
-//           <Typography>
-//             hi
-//           </Typography>
-//         </Toolbar>
-//       </AppBar> */}
-//       <Button variant='contained'>
-//         Hi,world
-//       </Button>
-//       <Typography variant='h5'>
-//         Hi, world
-//       </Typography>
-//     </>
-//   );
-// }
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import bgNavbar from '../../assets/bgNavbar.jpg';
+import iconMgtu from '../../assets/iconMgtu.png'
+import { makeStyles } from '@mui/styles';
+import { Box, Link } from '@mui/material';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+
+const useStyles = makeStyles((theme) => ({
+  navbarStyle: {
+    backgroundImage: `url(${bgNavbar})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center', 
+    color: 'white',
+    borderRadius: "20px",
+  },
+}));
 
 
 export default function Navbar() {
+  const classes = useStyles();
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+      <AppBar position="static" className={classes.navbarStyle}>
+        <Toolbar sx={{
+          px: '32px',
+          py: '20px',
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center'
+        }}>
+          
+          <Link href="https://olymp.bmstu.ru/ru" target='_blank'>
+            <img src={iconMgtu} alt="logoMgtu" />
+          </Link>
+
+          <Box sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            gap: '1.5vw'
+          }}>
+            <LanguageOutlinedIcon sx={{fontSize: 48}}></LanguageOutlinedIcon>
+            <SearchOutlinedIcon sx={{fontSize: 48}}></SearchOutlinedIcon>
+            <DarkModeOutlinedIcon sx={{fontSize: 48}}></DarkModeOutlinedIcon>
+            <AccountCircleOutlinedIcon sx={{fontSize: 48}}></AccountCircleOutlinedIcon>
+          </Box>
+          
         </Toolbar>
       </AppBar>
-    </Box>
   );
 }
