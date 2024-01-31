3648f87479f904b2aa492d094bc1af974f8eed99
goratsy <gorpetros06@gmail.com>
2024-01-31 23:36:11 +0300

    finishing first step in the client side. adding answers

--- src/components/catalog/Catalog.jsx
+++ src/components/catalog/Catalog.jsx
@@ -7,7 +7,9 @@ import SkipPreviousOutlinedIcon from '@mui/icons-material/SkipPreviousOutlined';
 import ButtonOutlined from "../buttonOutlined/buttonOutlined";
 import ButtonContained from "../buttonContained/buttonContained";
 import Alert from '@mui/material/Alert';
-import image from '../../assets/tasks/1.png';
+
+import imageTaskTest from '../../assets/tasks/1.png';
+import answerImageAnswerTest from '../../assets/tasks/answerImage/answer1.png';
 
 function Catalog() {
     const theme = useTheme();
@@ -28,11 +30,13 @@ function Catalog() {
         difficult: 'Легкая',
         yearTask: 2022,
         description: 'Два груза массами m1 и m2 связаны невесомой нерастяжимой нитью и находятся на горизонтальной поверхности. Коэффициенты трения между каждым грузом и поверхностью одинаковы. Если к грузу массой m1 приложить горизонтально направленную силу F1 (см. первый рисунок), то нить разорвется, когда F1 >= 10 Н. Если же горизонтально направленную силу F2 приложить к грузу массой m2 (см. второй рисунок), то нить разорвется, когда F2 >= 2,5 Н. Чему равно отношение масс грузов m1/m2? Ответ округлите до десятых',
-        image: image,
+        image: imageTaskTest,
+        answer: 4.0,
     }];
 
+ 
     let [showAlert, setShowAlert] = useState('none');
-
+    let [isShowAnswer, setIsShowAnswer] = useState(false)
 
     return(
         <Grid container spacing={3} columns={4}>
@@ -58,18 +62,48 @@ function Catalog() {
                     <img src={arrtask[0].image} alt='задача'/>
                     <Typography sx={{color: '#49454F'}}>{arrtask[0].description}</Typography>
 
-                    <TextField id="" label="Ответ" variant="outlined" sx={{mt: '24px'}}/>
-                    <Typography fontSize='small' sx={{color:'#B3261E', display: showAlert}}>😔  Неверный ответ. Проверьте решение</Typography>
-                    {/* <Alert icon={false} severity="warning" onClose={() => {console.log('closed');}}>😔  Неверный ответ. Проверьте решение</Alert> */}
-                    {/* <Alert icon={false} severity="success" onClose={() => {console.log('closed');}}>🥳  Правильный ответ</Alert> */}
+                    <Box sx={{mt: '12px',  width: '100%', display: (isShowAnswer ? 'block' : 'none')}}>
+                        <TextField id="" label="Ответ" variant="outlined"  sx={{width: '100%'}}/>
                         
-                   
-
-
-                    <Box sx={{display: 'flex', gap: '8px', justifyContent: 'flex-end', mt: '24px'}}>
-                        <ButtonOutlined>Показать решение</ButtonOutlined>
-                        <ButtonContained>Проверить ответ</ButtonContained>
+                        <Typography fontSize='small' sx={{color:'#B3261E', display: showAlert}}>😔  Неверный ответ. Проверьте решение</Typography>
+                        {/* <Alert icon={false} severity="warning" onClose={() => {console.log('closed');}}>😔  Неверный ответ. Проверьте решение</Alert> */}
+                        {/* <Alert icon={false} severity="success" onClose={() => {console.log('closed');}}>🥳  Правильный ответ</Alert> */}
+                            
+                        <Box sx={{display: 'flex', gap: '8px', justifyContent: 'flex-end', mt: '32px'}}>
+                            <ButtonOutlined>Показать решение</ButtonOutlined>
+                            <ButtonContained>Проверить ответ</ButtonContained>
+                        </Box>
                     </Box>
+
+                    <Box sx={{mt: '12px',  width: '100%', display: (isShowAnswer ? 'none' : 'block')}}>
+                        <Alert icon={false} severity="success" sx={{borderRadius: '12px'}} onClose={() => {console.log('closed');}}>🥳  Правильный ответ</Alert>
+                        <Box sx={{my: '16px'}}>
+                            <Typography sx={{color: 'black', fontSize: 'large', fontWeight: '500'}}>Решение</Typography>
+                            <Typography sx={{color: 'black', fontSize: '14px', color: '#49454F'}}>Ответ {arrtask[0].answer}</Typography>
+                        </Box>
+
+                        <Box sx={{color: '#49454F', fontSize: '14px', fontFamily: 'Roboto, sans-serif', display: 'flex', flexDirection: 'column', gap: '16px'}}>
+                            {/* Здесь распологается решение присланное из сервера */}
+                            <div>
+                                <p>Запишем уравнения динамики для каждого груза в первом случае.</p>
+                                <img src={answerImageAnswerTest} style={{width: '50%'}}/> 
+                            </div>
+                            <div>
+                                <p>Запишем уравнения динамики для каждого груза в первом случае.</p>
+                                <img src={answerImageAnswerTest} style={{width: '50%'}}/> 
+                            </div>
+                            <div>
+                                <p>Запишем уравнения динамики для каждого груза в первом случае.</p>
+                                <img src={answerImageAnswerTest} style={{width: '50%'}}/> 
+                            </div>
+                            
+                        </Box>
+
+                        <Box sx={{display: 'flex', gap: '8px', justifyContent: 'flex-end', mt: '32px'}}>
+                            <ButtonOutlined>Скрыть решение</ButtonOutlined>
+                        </Box>
+                    </Box>
+
                 </Box>
                 <Box>
                     <Box sx={{display: 'flex', flexDirection: 'row',  gap: '12px',}}>
